63da4cf4f8332ca204a2b4fa39f90fe16f4602db
Jaezon Sablay <sablaydanieljaezon@yahoo.com>
2018-05-28 15:43:43 +0800

    Version 1.0.4
    Fixed color scheme
    Added redirecting to next page (news)
    Fixed layout for services

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+
+	def phil
+
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'phil' => 'pages#phil'
+  #get '/pages', to: 'pages#phil', as: 'pages'
+  #resources :pages
 end
