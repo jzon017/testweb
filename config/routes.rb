e632eb7f665008ebd2043f649f034175ae250c97
Daryl D. Macadangdang <drylmcdngdng@gmail.com>
2018-06-01 16:30:55 +0800

    Version 1.0.9
    User Authentication added

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   root 'welcome#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'phil' => 'pages#phil'
+  #get 'phil' => 'pages#phil'
   get 'dtr' => 'employees#dtr'
+  get 'signin', to: 'sessions#show'
+  get 'signout', to: 'sessions#out'
+  post 'signin', to: 'sessions#create'
   #get 'employees/dtr'
   #get '/pages', to: 'pages#phil', as: 'pages'
   #resources :employees
