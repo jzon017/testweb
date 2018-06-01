6e93e7cbf2c0b7d4f27670f46cc5a8f56552ac72
Jaezon Sablay <sablaydanieljaezon@yahoo.com>
2018-06-01 14:48:32 +0800

    Version 1.0.8
    Added different navbars for different pages

---
+++ app/controllers/employees_controller.rb
@@ -0,0 +1,7 @@
+class EmployeesController < ApplicationController
+	def dtr
+		
+	end
+
+	
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'welcome#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'phil' => 'pages#phil'
+  get 'dtr' => 'employees#dtr'
+  #get 'employees/dtr'
   #get '/pages', to: 'pages#phil', as: 'pages'
-  #resources :pages
+  #resources :employees
 end
